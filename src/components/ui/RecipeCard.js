0b796046f7b727e416c13a3cd5aae180b9e9df63
Manuel <m_guzman74@yahoo.com>
2021-10-24 00:16:16 -0500

    nullish coalescing used to provice placeholder if api src failed

--- src/components/ui/RecipeCard.js
+++ src/components/ui/RecipeCard.js
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from "./RecipeCard.module.css";
-
+import imgPlaceHolder from "../../assets/no-image-placeholder.png";
 const RecipeCard = ({ title, imgSrc }) => {
   return (
     <div data-testid="main-wrapper" className={styles.mainWrapper}>
       <div data-testid="image-test" className={styles.imageWrapper}>
-        <img src={imgSrc} alt={`Still of ${title}`} />
+        <img src={imgSrc ?? imgPlaceHolder} alt={`Still of ${title}`} />
       </div>
+
       <div data-testid="info-test"></div>
     </div>
   );
--- src/components/ui/RecipeCard.test.js
+++ src/components/ui/RecipeCard.test.js
@@ -3,6 +3,8 @@ import { screen, render } from "@testing-library/react";
 import RecipeCard from "./RecipeCard";
 import recipeMock from "../../mock/recipeMock";
 
+import imgPlaceHolder from "../../assets/no-image-placeholder.png";
+
 const baseApiRecipe = recipeMock.results[0];
 // console.log(baseApiRecipe);
 
@@ -26,5 +28,13 @@ test("it renders an image", () => {
     <RecipeCard title={baseApiRecipe.title} imgSrc={baseApiRecipe.image} />
   );
   const image = screen.getByRole("img");
+  // console.log(image);
   expect(image).toHaveAttribute("src", baseApiRecipe.image);
+});
+
+test("if no image or api image error, render placeholder", () => {
+  render(
+    <RecipeCard title={baseApiRecipe.title} imgSrc={"error || api bug"} />
+  );
+  expect(<RecipeCard title={baseApiRecipe.title} imgSrc={imgPlaceHolder} />);
 });
